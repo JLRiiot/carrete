8236efabb735b0d34b76eee4fb1ef75bb67705f4
José Luis Montaña Hernandez <jmontana@xceleration.com>
2019-04-05 01:05:17 -0500

    Updates

---
+++ precache-manifest.69101e1d6289525af6ad07be6ae4ad41.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "d0c6875f1f9d6c5e3134",
+    "url": "/carrete/static/js/runtime~main.aee44d45.js"
+  },
+  {
+    "revision": "a837b6c41c872b6b5925",
+    "url": "/carrete/static/js/main.7f964ecb.chunk.js"
+  },
+  {
+    "revision": "7e6e8118b0aa6f6eaa4f",
+    "url": "/carrete/static/js/2.64fea4df.chunk.js"
+  },
+  {
+    "revision": "f0906f5ba9596166377a3a95cd521009",
+    "url": "/carrete/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/JLRiiot/carrete/precache-manifest.d69ed6214ec4d8b1642066805c7705fa.js"
+  "/carrete/precache-manifest.69101e1d6289525af6ad07be6ae4ad41.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/JLRiiot/carrete/index.html", {
+workbox.routing.registerNavigationRoute("/carrete/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
